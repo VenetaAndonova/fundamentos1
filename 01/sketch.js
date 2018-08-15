a0b8135aff732ed59bd9a6b88d193c8e3aec69e9
VenetaAndonova <42419711+VenetaAndonova@users.noreply.github.com>
2018-08-15 14:33:19 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,69 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(238,232,170);
+var c = color (75,0,130, 191);
+fill (c);
+noStroke ()
+ellipse(54,293,107,107)
+var c = color (143,188,143,191);
+fill (c);
+noStroke ()
+ellipse(280,354,208,208)
+var c = color (139,0,0);
+fill (c);
+stroke(0,0,0)
+rect(375,400,77,69)
+line(423,432, 477, 432)
+var c = color (0,0,0);
+fill (c);
+stroke(0,0,0)
+rect(423,440,55,3)
+var c = color(204,204,0)
+fill (c)
+noStroke()
+triangle(154,277,285,123,331,331)
+angleMode(degrees) 
+var c = color(218,165,32)
+fill (c)
+noStroke()
+triangle(314,255,196,291,331,331)    
+  arc(280,354,208,208,110.6,275.25,OPEN);
+stroke(0,0,0)
+  line(285,123, 0,123);
+stroke(0,0,0)
+line(247,0,585,524);
+var c=color(218,165,32)
+fill (c)
+stroke(0,0,0)
+triangle(285,0,300,0,385,131);
+var c=color(218,165,32)
+fill (c)
+stroke(0,0,0)
+triangle(331,0,340,0,360,39);
+var c=color(218,165,32)
+fill (c)
+stroke(0,0,0)
+triangle(392,0,400,0,414,23);
+
+var c=color(255,255,255)
+fill (c)
+stroke(0,0,0)
+arc(0,122,185,185, PI+QUARTER_PI,0);
+stroke(0,0,0);
+line(0,290,254,290);
+stroke(0,0,0)
+line(0,424,270,424);
+var c=color(0,0,0)
+fill (c)
+stroke (0,0,0);
+beginShape()
+vertex(0,30)
+vertex(0,35)
+vertex(308,339)
+vertex(312,340)
+endShape();
+}
+ 
